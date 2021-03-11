9c169bebf2cac63571c5a381b10d719b7f091d91
mr838wind <mr838wind@gmail.com>
2021-03-11 22:43:12 +0900

    [mod] service에 selectByPk method 추가

--- shell/sync.sh
+++ shell/sync.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # copy 00-wind-gen groovy files to datagrip Extenshions > schema
+# windows: install git-bash
+##  https://violetboralee.medium.com/intellij-idea%EC%99%80-git-bash-%EC%97%B0%EB%8F%99%ED%95%98%EA%B8%B0-63e8216aa7de
 
 ## property
+##_OUT_DIR=C:/Users/mr838/AppData/Roaming/JetBrains/DataGrip2020.3/extensions/com.intellij.database/schema/
 _OUT_DIR=/Users/wind/Library/Preferences/DataGrip2019.3/extensions/com.intellij.database/schema/
 _IN_DIR=$(pwd)/../
 
